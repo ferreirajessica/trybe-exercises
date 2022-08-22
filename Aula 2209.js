f6a437dac783df5eb37e5e75ed55e39c694c03cf
Jessica Ferreira <samojess@gmail.com>
2022-08-22 19:00:35 -0300

    fim dos exercicios

---
+++ Aula 2209.js
@@ -0,0 +1,21 @@
+// 1 - 
+// Faça um algoritmo que calcule a soma de 1 a 50 usando a estrutura "for" e retorne no formato:
+//A soma total de 1 a 50 é:
+
+
+
+let sum = 0;
+
+for (let i = 0; i <= 50; i += 1) {
+sum = sum + i; 
+}
+
+console.log('A soma total de 1 a 50 é ' + sum + '.');
+
+
+let num = 100;
+for (let j = 0; j < 20; j += 1) {
+num -= 5;
+};
+
+console.log(num);
