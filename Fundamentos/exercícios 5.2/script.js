1bb8e24910ca7d22557610ba5d6185ea63fe9a0a
Jessica Ferreira <samojess@gmail.com>
2022-08-26 18:38:15 -0300

    starts day 5.2

---
+++ Fundamentos/exercícios 5.2/script.js
@@ -0,0 +1,5 @@
+// arquivo script.js
+
+console.log(document.getElementById('primeiroFilho').nextElementSibling);
+console.log(document.getElementById('primeiroFilho').nextElementSibling.parentElement).style.backgroundColor = 'pink';
+document.getElementById('primeiroFilho').nextElementSibling.parentElement.style.backgroundColor = 'pink';
