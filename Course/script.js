0a387a33447d7a20be2d6e268c860613622e8af3
Jessica Ferreira <samojess@gmail.com>
2022-08-29 18:45:40 -0300

    exercicios de acesso

--- Course/script.js
+++ Course/script.js
@@ -8,3 +8,13 @@ let atencao = ondeVoceEsta.parentNode.childNodes[4];
 let terceiroFilho = ondeVoceEsta.nextElementSibling;
 let terceiroFilhodePai = paiDeOndeVoceEsta.children[2];
 
+let novoIrmao = document.createElement('section');
+paiDeOndeVoceEsta.appendChild(novoIrmao);
+novoIrmao.id = 'novoIrmao';
+
+let novoFilho = document.createElement('section');
+ondeVoceEsta.appendChild(novoFilho);
+novoFilho.id = 'novoFilho';
+
+let acesseTerceiro = novoFilho.parentElement.nextElementSibling;
+
