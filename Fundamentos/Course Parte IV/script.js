54e1eb4bae89d8d5fd2c9911abad2c7e3653f1dc
Jessica Ferreira <samojess@gmail.com>
2022-09-08 14:35:11 -0300

    fim do dia 1

---
+++ Fundamentos/Course Parte IV/script.js
@@ -0,0 +1,18 @@
+// SELETORES
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+function prevent(event) {
+event.preventDefault();
+}
+
+function prevent2 (event) {
+    if (event.key !== "a"){
+        event.preventDefault();
+    }
+}
+
+HREF_LINK.addEventListener('click', prevent);
+INPUT_CHECKBOX.addEventListener('click', prevent);
+INPUT_TEXT.addEventListener('keypress', prevent2);
